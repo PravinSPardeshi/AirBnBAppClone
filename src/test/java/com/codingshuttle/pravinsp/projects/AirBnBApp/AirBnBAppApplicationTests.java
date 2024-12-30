4a8aa3d68b480bcd25981daf095af92137960e53
PravinSPardeshi <pravinpardeshi100@gmail.com>
2024-12-30 16:14:38 +0530

    adding the initial files

---
+++ src/test/java/com/codingshuttle/pravinsp/projects/AirBnBApp/AirBnBAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.pravinsp.projects.AirBnBApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnBAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
